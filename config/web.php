a5039460173b529553e978c7f7b5abb64db89bb6
Андрей Анучкин <uniqcle@yandex.ru>
2018-09-23 22:03:22 +0600

    tuning: .htaccess

--- config/web.php
+++ config/web.php
@@ -15,6 +15,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'EaWsN33ubYBslZXtmh6BSwoF18adxTTL',
+            'baseUrl'=> '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -43,14 +44,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
+        
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+         
     ],
     'params' => $params,
 ];
